7f6c7ce213c0dfd45bda409c19858faa54c1d5e9
Diego Davila <diego.davila.ddc@gmail.com>
2025-01-10 20:15:13 -0500

    remover moneda de formatter

--- app/src/main/java/com/avoqado/pos/core/utils/AmountStringExt.kt
+++ app/src/main/java/com/avoqado/pos/core/utils/AmountStringExt.kt
@@ -2,7 +2,7 @@ package com.avoqado.pos.core.utils
 
 fun String.toAmountMx(): String {
     val amount = this.toDouble() / 100
-    return "MXN %.2f".format(amount)
+    return "%.2f".format(amount)
 }
 
 fun String.toAmountMXDouble(): Double {
