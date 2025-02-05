476872ad1fc224b43e7a75f1e0439e653941eba0
Diego Davila <diego.davila.ddc@gmail.com>
2025-02-05 18:03:33 -0500

    fix: agregar dependencia faltante

--- app/src/main/java/com/avoqado/pos/features/payment/presentation/navigation/PaymentNavigation.kt
+++ app/src/main/java/com/avoqado/pos/features/payment/presentation/navigation/PaymentNavigation.kt
@@ -1,6 +1,7 @@
 package com.avoqado.pos.features.payment.presentation.navigation
 
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavGraphBuilder
 import com.avoqado.pos.OperationFlowHolder
 import com.avoqado.pos.core.data.local.SessionManager
@@ -13,6 +14,7 @@ import com.avoqado.pos.features.payment.presentation.inputTipAmount.InputTipView
 import com.avoqado.pos.features.payment.presentation.paymentResult.PaymentResultScreen
 import com.avoqado.pos.features.payment.presentation.paymentResult.PaymentResultViewModel
 import com.avoqado.pos.router.composableHolder
+import com.menta.android.printer.i9100.core.DevicePrintImpl
 
 fun NavGraphBuilder.paymentNavigation(navigationDispatcher: NavigationDispatcher, sessionManager: SessionManager) {
     composableHolder(MainDests.InputTip) {
@@ -33,6 +35,7 @@ fun NavGraphBuilder.paymentNavigation(navigationDispatcher: NavigationDispatcher
     }
 
     composableHolder(MainDests.PaymentResult) {
+        val context = LocalContext.current
         val paymentResultViewModel = remember {
             PaymentResultViewModel(
                 navigationDispatcher = navigationDispatcher,
@@ -40,6 +43,9 @@ fun NavGraphBuilder.paymentNavigation(navigationDispatcher: NavigationDispatcher
                 sessionManager = sessionManager
             )
         }
-        PaymentResultScreen(paymentResultViewModel)
+        PaymentResultScreen(
+            paymentResultViewModel,
+            DevicePrintImpl(context)
+        )
     }
 }
